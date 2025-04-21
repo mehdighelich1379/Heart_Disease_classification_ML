9afd339a46a85783cd95018b81b34129e2c4bee2
Mehdi Ghelich <qelejkhanimehdi@gmail.com>
2025-04-21 13:48:22 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ import pandas as pd
 
 
 
-with open('./models/svc_Model.txt', 'rb') as model_file:
+with open('catboost_model.txt', 'rb') as model_file:
     model = pickle.load(model_file)
-st.write("Enter the details for prediction:")
+st.sidebar.header("Enter the details for prediction:")
 
-age = st.number_input('Age', min_value=1, max_value=120, value=60)
-sex = st.selectbox('Sex', options=[0, 1], format_func=lambda x: 'Male' if x == 1 else 'Female')
-cp = st.selectbox('CP', options=[0, 1, 2, 3])
-trestbps = st.number_input('Resting Blood Pressure', min_value=0, max_value=300, value=120)
-chol = st.number_input('Cholesterol', min_value=0, max_value=600, value=200)
+age = st.sidebar.number_input("inter your age", min_value=0, max_value=100, value=0)
+sex = st.sidebar.number_input("inter your sex", min_value=0, max_value=1, value=0)
+cp = st.sidebar.number_input("inter your cp", min_value=0, max_value=3, value=0)
+trestbps = st.sidebar.number_input("inter your trestbps", min_value=0, max_value=300, value=0)
+chol = st.sidebar.number_input("inter your chol", min_value=0, max_value=500, value=0)
+restecg = st.sidebar.number_input("inter your restecg", min_value=0, max_value=2, value=0)
+thalach = st.sidebar.number_input("inter your thalach", min_value=0, max_value=200, value=0)
+exang = st.sidebar.number_input("inter your exang", min_value=0, max_value=1, value=0)
+oldpeak = st.sidebar.number_input("inter your oldpeak", min_value=0, max_value=5, value=0)
+slope = st.sidebar.number_input("inter your slope", min_value=0, max_value=2, value=0)
+ca = st.sidebar.number_input("inter your ca", min_value=0, max_value=4, value=0)
+thal = st.sidebar.number_input("inter your thal", min_value=0, max_value=3, value=0)
 
-restecg = st.selectbox('Resting Electrocardiographic Results', options=[0, 1, 2])
-thalach = st.number_input('Maximum Heart Rate Achieved', min_value=0, max_value=250, value=150)
-exang = st.selectbox('Exercise Induced Angina', options=[0, 1])
-oldpeak = st.number_input('Oldpeak', value=0.0)
-slope = st.selectbox('Slope of the Peak Exercise ST Segment', options=[0, 1, 2])
-ca = st.selectbox('Number of Major Vessels Colored by Fluoroscopy', options=[0, 1, 2, 3])
-thal = st.selectbox('Thalassemia', options=[0, 1, 2, 3])
 
-# ایجاد دیتا فریم برای ورودی‌های جدید
 user_input = pd.DataFrame({
     'age': [age],
     'sex': [sex],
@@ -38,12 +37,12 @@ user_input = pd.DataFrame({
     'thal': [thal]
 })
 
-# پیش‌بینی با استفاده از مدل
-if 'model' in locals():
-    prediction = model.predict(user_input)
-    
-    # نمایش نتیجه پیش‌بینی
-    if prediction[0] == 1:
-        st.write('Prediction: Heart Disease Detected')
+prob = model.predict_proba(user_input)[0][1]
+
+st.sidebar.write(f"Probability of Heart Disease: {prob:.2f}")
+
+if st.button("Predict"):
+    if prob >= 0.6:
+        st.error("The patient has a high probability of having heart disease.")
     else:
-        st.write('Prediction: No Heart Disease')
+        st.success("The patient has a low probability of having heart disease.")
